8ec3802e23b312e063280fca5c47e7b1dbc7acfc
Owen1Campbell <ocampbell@knights.ucf.edu>
2022-04-12 23:03:33 -0400

    Update functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -1,1 +1,23 @@
 <?php 
+
+function emptyInputSignup($name, $email, $username, $pass, $passRepeat) {
+    $result;
+    if (empty($name) || empty($email) || empty($username) || empty($pass) || empty($passRepeat)) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
+
+function invalidUid($username) {
+    $result;
+    if (preg_match()) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
