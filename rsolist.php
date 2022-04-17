f9b591219e5172e886b6d12a984127de70cc34bd
Owen1Campbell <ocampbell@knights.ucf.edu>
2022-04-17 15:46:47 -0400

    Update rsolist.php

--- rsolist.php
+++ rsolist.php
@@ -2,7 +2,25 @@
     include_once 'header.php'; 
     include_once 'includes/dbh.inc.php';
 ?>
-<h1>RSOs at [University]</h1>
+<h2>RSOs at 
+    <?php 
+        $sql = "SELECT * FROM university WHERE universityId = ?;";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+          header("location: ../rsolist.php?error=stmtfail");
+          exit();
+        }
+    
+        mysqli_stmt_bind_param($stmt, "i", $_SESSION["useruniv"]);
+        mysqli_stmt_execute($stmt);
+    
+        $resultData = mysqli_stmt_get_result($stmt);
+    
+        $uuRow = mysqli_fetch_assoc($resultData);
+        mysqli_stmt_close($stmt);
+        echo $uuRow["universityName"];
+    ?>
+</h2>
 <div class='createdmsg'>
     <?php
         if (isset($_GET["create"])) {
